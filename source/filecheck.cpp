83721e62c29b82f44a208ee3298d560afa3b2fa3
Daniel <danielga@outlook.pt>
2015-10-29 15:34:50 +0000

    Modified filepath for download validation to have 3 modes.
    Off, module side (checks downloadables stringtable and others) or Lua side (calls the IsValidFileForTransfer hook).
    Cleaned up code a bit.

--- source/filecheck.cpp
+++ source/filecheck.cpp
@@ -1,3 +1,4 @@
+#include <GarrysMod/Lua/LuaInterface.h>
 #include <filecheck.hpp>
 #include <main.hpp>
 #include <cstdint>
@@ -13,17 +14,17 @@ namespace filecheck
 #if defined _WIN32
 
 static const char *IsValidFileForTransfer_sig = "\x55\x8B\xEC\x8B\x4D\x08\x85\xC9\x0F\x84\x2A\x2A\x2A\x2A\x80\x39";
-static size_t IsValidFileForTransfer_siglen = 16;
+static const size_t IsValidFileForTransfer_siglen = 16;
 
 #elif defined __linux
 
 static const char *IsValidFileForTransfer_sig = "@_ZN8CNetChan22IsValidFileForTransferEPKc";
-static size_t IsValidFileForTransfer_siglen = 0;
+static const size_t IsValidFileForTransfer_siglen = 0;
 
 #elif defined __APPLE__
 
 static const char *IsValidFileForTransfer_sig = "@__ZN8CNetChan22IsValidFileForTransferEPKc";
-static size_t IsValidFileForTransfer_siglen = 0;
+static const size_t IsValidFileForTransfer_siglen = 0;
 
 #endif
 
@@ -34,6 +35,57 @@ static MologieDetours::Detour<IsValidFileForTransfer_t> *IsValidFileForTransfer_
 
 static INetworkStringTable *downloads = nullptr;
 static const char *downloads_dir = "downloads" CORRECT_PATH_SEPARATOR_S;
+
+static GarrysMod::Lua::ILuaInterface *lua_interface = nullptr;
+static uint32_t validation_mode = 0;
+static const char *hook_name = "IsValidFileForTransfer";
+
+static bool PushDebugTraceback( GarrysMod::Lua::ILuaInterface *lua )
+{
+	lua->GetField( GarrysMod::Lua::INDEX_GLOBAL, "debug" );
+	if( !lua->IsType( -1, GarrysMod::Lua::Type::TABLE ) )
+	{
+		lua->ErrorNoHalt( "[ServerSecure] Global debug is not a table!\n" );
+		lua->Pop( 1 );
+		return false;
+	}
+
+	lua->GetField( -1, "traceback" );
+	lua->Remove( -2 );
+	if( !lua->IsType( -1, GarrysMod::Lua::Type::FUNCTION ) )
+	{
+		lua->ErrorNoHalt( "[ServerSecure] Global debug.traceback is not a function!\n" );
+		lua->Pop( 1 );
+		return false;
+	}
+
+	return true;
+}
+
+inline bool PushHookRun( GarrysMod::Lua::ILuaInterface *lua )
+{
+	if( !PushDebugTraceback( lua ) )
+		return false;
+
+	lua->GetField( GarrysMod::Lua::INDEX_GLOBAL, "hook" );
+	if( !lua->IsType( -1, GarrysMod::Lua::Type::TABLE ) )
+	{
+		lua->ErrorNoHalt( "[ServerSecure] Global hook is not a table!\n" );
+		lua->Pop( 2 );
+		return false;
+	}
+
+	lua->GetField( -1, "Run" );
+	lua->Remove( -2 );
+	if( !lua->IsType( -1, GarrysMod::Lua::Type::FUNCTION ) )
+	{
+		lua->ErrorNoHalt( "[ServerSecure] Global hook.Run is not a function!\n" );
+		lua->Pop( 2 );
+		return false;
+	}
+
+	return true;
+}
 
 inline bool BlockDownload( const char *filepath )
 {
@@ -49,18 +101,37 @@ static bool IsValidFileForTransfer_d( const char *filepath )
 		return false;
 	}
 
-	size_t len = strlen( filepath );
+	size_t len = std::strlen( filepath );
 	if( len == 0 )
 	{
 		DebugWarning( "[ServerSecure] Invalid file to download (path length was 0)\n" );
 		return false;
 	}
 
+	if( validation_mode == 2 )
+	{
+		if( !PushHookRun( lua_interface ) )
+			return false;
+
+		lua_interface->PushString( hook_name );
+		lua_interface->PushString( filepath );
+
+		bool valid = true;
+		if( lua_interface->PCall( 2, 1, -4 ) != 0 )
+			lua_interface->Msg( "\n[ERROR] %s\n\n", lua_interface->GetString( -1 ) );
+		else if( lua_interface->IsType( -1, GarrysMod::Lua::Type::BOOL ) )
+			valid = lua_interface->GetBool( -1 );
+
+		lua_interface->Pop( 2 );
+
+		return valid;
+	}
+
 	std::string nicefile( filepath, len );
 	if( !V_RemoveDotSlashes( &nicefile[0] ) )
 		return BlockDownload( filepath );
 
-	len = strlen( nicefile.c_str( ) );
+	len = std::strlen( nicefile.c_str( ) );
 	nicefile.resize( len );
 	filepath = nicefile.c_str( );
 
@@ -73,41 +144,49 @@ static bool IsValidFileForTransfer_d( const char *filepath )
 	if( index != INVALID_STRING_INDEX )
 		return true;
 
-	if( len == 22 && strncmp( filepath, downloads_dir, 10 ) == 0 && strncmp( filepath + len - 4, ".dat", 4 ) == 0 )
+	if( len == 22 && std::strncmp( filepath, downloads_dir, 10 ) == 0 && std::strncmp( filepath + len - 4, ".dat", 4 ) == 0 )
 		return true;
 
 	return BlockDownload( filepath );
 }
 
-LUA_FUNCTION_STATIC( EnableFileValidation )
+inline bool SetDetourStatus( uint32_t mode )
 {
-	LUA->CheckType( 1, GarrysMod::Lua::Type::BOOL );
-
-	bool detour = LUA->GetBool( 1 );
-	if( detour && IsValidFileForTransfer_detour == nullptr )
+	if( ( mode == 1 || mode == 2 ) && IsValidFileForTransfer_detour == nullptr )
 	{
 		IsValidFileForTransfer_detour = new( std::nothrow ) MologieDetours::Detour<IsValidFileForTransfer_t>(
 			IsValidFileForTransfer,
 			IsValidFileForTransfer_d
 		);
-		LUA->PushBool( IsValidFileForTransfer_detour != nullptr );
+		if( IsValidFileForTransfer_detour != nullptr )
+		{
+			validation_mode = mode;
+			return true;
+		}
 	}
-	else if( !detour && IsValidFileForTransfer_detour != nullptr )
+
+	if( mode == 0 && IsValidFileForTransfer_detour != nullptr )
 	{
 		delete IsValidFileForTransfer_detour;
 		IsValidFileForTransfer_detour = nullptr;
-		LUA->PushBool( true );
-	}
-	else
-	{
-		LUA->PushBool( false );
+		validation_mode = mode;
+		return true;
 	}
 
+	return false;
+}
+
+LUA_FUNCTION_STATIC( EnableFileValidation )
+{
+	LUA->CheckType( 1, GarrysMod::Lua::Type::NUMBER );
+	LUA->PushBool( SetDetourStatus( static_cast<uint32_t>( LUA->GetNumber( 1 ) ) ) );
 	return 1;
 }
 
 void Initialize( lua_State *state )
 {
+	lua_interface = static_cast<GarrysMod::Lua::ILuaInterface *>( LUA );
+
 	INetworkStringTableContainer *networkstringtable = global::engine_loader.GetInterface<INetworkStringTableContainer>(
 		INTERFACENAME_NETWORKSTRINGTABLESERVER
 	);
