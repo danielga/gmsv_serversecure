7feb5f4889ff2a3371ea130153258ce962cf210d
Daniel <danielga@outlook.pt>
2016-04-23 18:36:33 +0100

    Bumped up revision again for minor recent changes.

--- source/main.cpp
+++ source/main.cpp
@@ -22,11 +22,11 @@ static void PreInitialize( lua_State *state )
 
 	LUA->CreateTable( );
 
-	LUA->PushString( "serversecure 1.3.2" );
+	LUA->PushString( "serversecure 1.3.3" );
 	LUA->SetField( -2, "Version" );
 
 	// version num follows LuaJIT style, xxyyzz
-	LUA->PushNumber( 10302 );
+	LUA->PushNumber( 10303 );
 	LUA->SetField( -2, "VersionNum" );
 }
 
