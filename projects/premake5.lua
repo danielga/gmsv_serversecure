3957d39a1e6a56b1784f766281af487a855a1255
Daniel <danielga@outlook.pt>
2016-05-02 10:58:41 +0100

    More Mac OSX fixes, GCC/G++ 4.9 on Linux, readme update.

--- projects/premake5.lua
+++ projects/premake5.lua
@@ -1,21 +1,21 @@
 if os.is("windows") and _ACTION ~= "vs2010" then
-	error("The only supported compilation platform for this project on Windows is Visual Studio 2010 (for ABI reasons).")
+	error("The only supported compilation platform for this project on Windows is Visual Studio 2010.")
 elseif os.is("linux") then
-	print("WARNING: The only supported compilation platform (tested) for this project is GCC/G++ 4.8. However, any version between 4.4 and 4.9 *MIGHT* work.")
+	print("WARNING: The only supported compilation platforms (tested) for this project on Linux are GCC/G++ 4.8 or 4.9. However, any version between 4.4 and 4.9 *MIGHT* work.")
 elseif os.is("macosx") then
-	print("WARNING: The only supported compilation platform (tested) for this project is Xcode 4.1. However, any Xcode version *MIGHT* work as long as the Mac OSX 10.5 SDK is used (https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.5.sdk.tar.xz).")
+	print("WARNING: The only supported compilation platform (tested) for this project on Mac OSX is Xcode 4.1. However, any Xcode version *MIGHT* work as long as the Mac OSX 10.5 SDK is used (https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.5.sdk.tar.xz).")
 
 	newoption({
 		trigger = "macosxsdk",
 		description = "Sets the path to the Mac OSX 10.5 SDK (https://github.com/phracker/MacOSX-SDKs) directory",
-		value = "path to Mac OSX 10.5 SDK dir"
+		value = "path to Mac OSX 10.5 SDK directory"
 	})
 end
 
 newoption({
 	trigger = "gmcommon",
 	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
-	value = "path to garrysmod_common dir"
+	value = "path to garrysmod_common directory"
 })
 
 local gmcommon = _OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON")
