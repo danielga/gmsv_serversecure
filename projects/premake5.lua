1978a1cf4a867c7c5ac3f0a1d623a09831e3d71e
Daniel <danielga@outlook.pt>
2016-05-03 03:04:37 +0100

    Let's try this again, now with even more Mac OSX 10.6 SDK and --sysroot!

--- projects/premake5.lua
+++ projects/premake5.lua
@@ -3,12 +3,12 @@ if os.is("windows") and _ACTION ~= "vs2010" then
 elseif os.is("linux") then
 	print("WARNING: The only supported compilation platforms (tested) for this project on Linux are GCC/G++ 4.8 or 4.9. However, any version between 4.4 and 4.9 *MIGHT* work.")
 elseif os.is("macosx") then
-	print("WARNING: The only supported compilation platform (tested) for this project on Mac OSX is Xcode 4.1. However, any Xcode version *MIGHT* work as long as the Mac OSX 10.5 SDK is used (https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.5.sdk.tar.xz).")
+	print("WARNING: The only supported compilation platform (tested) for this project on Mac OSX is Xcode 4.1. However, any Xcode version *MIGHT* work as long as the Mac OSX 10.6 SDK is used (https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.6.sdk.tar.xz).")
 
 	newoption({
 		trigger = "macosxsdk",
-		description = "Sets the path to the Mac OSX 10.5 SDK (https://github.com/phracker/MacOSX-SDKs) directory",
-		value = "path to Mac OSX 10.5 SDK directory"
+		description = "Sets the path to the Mac OSX 10.6 SDK (https://github.com/phracker/MacOSX-SDKs) directory",
+		value = "path to Mac OSX 10.6 SDK directory"
 	})
 end
 
@@ -38,5 +38,5 @@ CreateSolution({name = "serversecure", allow_debug = false})
 		filter("system:macosx")
 			buildoptions({
 				"-mmacosx-version-min=10.5",
-				"-isysroot " .. (_OPTIONS.macosxsdk or os.getenv("MACOSX_SDK") or path.getabsolute("macosx/MacOSX10.5.sdk"))
+				"--sysroot=" .. (_OPTIONS.macosxsdk or os.getenv("MACOSX_SDK") or path.getabsolute("macosx/MacOSX10.6.sdk"))
 			})
