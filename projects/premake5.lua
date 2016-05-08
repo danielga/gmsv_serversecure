caeeb5fab85c26fdeb2cc991cfacac6cac6ef909
Daniel <danielga@outlook.pt>
2016-05-08 20:21:27 +0100

    Hopefully fixed Mac OS X compilation for good?
    No more need for "special" SDKs (just set the min version flag).

--- projects/premake5.lua
+++ projects/premake5.lua
@@ -3,13 +3,7 @@ if os.is("windows") and _ACTION ~= "vs2010" then
 elseif os.is("linux") then
 	print("WARNING: The only supported compilation platforms (tested) for this project on Linux are GCC/G++ 4.8 or 4.9. However, any version between 4.4 and 4.9 *MIGHT* work.")
 elseif os.is("macosx") then
-	print("WARNING: The only supported compilation platform (tested) for this project on Mac OSX is Xcode 4.1. However, any Xcode version *MIGHT* work as long as the Mac OSX 10.6 SDK is used (https://github.com/phracker/MacOSX-SDKs/releases/download/MacOSX10.11.sdk/MacOSX10.6.sdk.tar.xz).")
-
-	newoption({
-		trigger = "macosxsdk",
-		description = "Sets the path to the Mac OSX 10.6 SDK (https://github.com/phracker/MacOSX-SDKs) directory",
-		value = "path to Mac OSX 10.6 SDK directory"
-	})
+	print("WARNING: The only supported compilation platform (tested) for this project on Mac OSX is Xcode 4.1. However, any Xcode version *MIGHT* work as long as the Mac OSX 10.5 SDK is used (-mmacosx-version-min).")
 end
 
 newoption({
@@ -26,6 +20,10 @@ end
 include(gmcommon)
 
 CreateSolution({name = "serversecure", allow_debug = false})
+	filter("system:macosx")
+		buildoptions("-mmacosx-version-min=10.5")
+		linkoptions("-mmacosx-version-min=10.5")
+
 	CreateProject({serverside = true})
 		warnings("Default")
 		IncludeSDKCommon()
@@ -34,9 +32,3 @@ CreateSolution({name = "serversecure", allow_debug = false})
 		IncludeSteamAPI()
 		IncludeDetouring()
 		IncludeScanning()
-
-		filter("system:macosx")
-			buildoptions({
-				"-mmacosx-version-min=10.5",
-				"--sysroot=" .. (_OPTIONS.macosxsdk or os.getenv("MACOSX_SDK") or path.getabsolute("macosx/MacOSX10.6.sdk"))
-			})
