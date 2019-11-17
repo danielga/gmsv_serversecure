0c0c8ed00aa912f95375b1e71afddfc84243f169
Daniel <danielga@outlook.pt>
2019-11-17 23:17:26 +0000

    Remove redundant Color.h include on main.hpp

--- source/main.hpp
+++ source/main.hpp
@@ -7,7 +7,6 @@
 #if defined DEBUG
 
 #include <dbg.h>
-#include <Color.h>
 
 #define _DebugMsg( ... ) Msg( __VA_ARGS__ )
 #define _DebugWarning( ... ) ConColorMsg( 1, global::__yellow, __VA_ARGS__ )
